22ba57a219ce159088d66a65557062eebc643822
Saurabh <saurabhsuryan99@hotmail.com>
2020-02-09 15:42:22 +0530

    Updates

---
+++ precache-manifest.74027b8048b609509ceee279c6c1f17d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34bec27396288996e4e1b0552e5a08ea",
+    "url": "/mytodo/index.html"
+  },
+  {
+    "revision": "2e4492380b6e204f2373",
+    "url": "/mytodo/static/css/main.f3e83cb9.chunk.css"
+  },
+  {
+    "revision": "3cc6262f6462cd7a4848",
+    "url": "/mytodo/static/js/2.85f20517.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mytodo/static/js/2.85f20517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e4492380b6e204f2373",
+    "url": "/mytodo/static/js/main.f28bff64.chunk.js"
+  },
+  {
+    "revision": "06142494a24f27c32beb",
+    "url": "/mytodo/static/js/runtime-main.7d38feb6.js"
+  },
+  {
+    "revision": "b539e89c05f9b79adee4927d9fc4507a",
+    "url": "/mytodo/static/media/enter.b539e89c.svg"
+  }
+]);
